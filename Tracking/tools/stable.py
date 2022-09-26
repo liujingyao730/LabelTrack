809fed9e82ddb5b6d54e952b1bc96d67fa564464
Yinglong Zhu <z.yinglong@outlook.com>
2022-09-26 11:02:53 +0800

    1st version of stable and ground coordinate.

---
+++ Tracking/tools/stable.py
@@ -0,0 +1,240 @@
+import time
+import cv2
+import numpy as np
+
+# TODO: refactor this small tool to increase the efficiency
+
+
+class Stable:
+    __video_path = None
+
+    __surf = {
+        # TODO: SURF is little bit too old for this project
+        'surf': None,
+        'kp': None,
+        'des': None,
+        'template_kp': None
+    }
+
+    # capture
+    __capture = {
+        'cap': None,
+        'size': None,
+        'frame_count': None,
+        'fps': None,
+        'video': None
+    }
+
+    __config = {
+        # TODO: if the count is decreased, the time will not be decrease.
+        'key_point_count': 5000,
+        'index_params': dict(algorithm=0, trees=5),
+        'search_params': dict(checks=50),
+        'ratio': 0.5,
+        'frame_count': 20000
+    }
+
+    __current_frame = 0
+    __handle_count = 0
+    __handle_timer = {
+        'init': 0,
+        'handle': 0,
+        'read': 0,
+        'key': 0,
+        'matrix': 0,
+        'flann': 0,
+        'perspective': 0,
+        'write': 0,
+        'other': 0,
+    }
+
+    def __init__(self, capture:cv2.VideoCapture=None) -> None:
+        self.__init_capture(capture)
+        self.__init_surf()
+
+    def __init_capture(self, capture=None):
+        if capture is None:
+            self.__capture['cap'] = cv2.VideoCapture(self.__video_path)
+        self.__capture['cap'] = capture
+        self.__capture['size'] = (int(self.__capture['cap'].get(cv2.CAP_PROP_FRAME_WIDTH)),
+                                  int(self.__capture['cap'].get(cv2.CAP_PROP_FRAME_HEIGHT)))
+
+        self.__capture['fps'] = self.__capture['cap'].get(cv2.CAP_PROP_FPS)
+
+        self.__capture['video'] = cv2.VideoWriter(self.__video_path.replace('.', '_stable.'),
+                                                  cv2.VideoWriter_fourcc(*"mp4v"),
+                                                  self.__capture['fps'],
+                                                  self.__capture['size'])
+
+        self.__capture['frame_count'] = int(self.__capture['cap'].get(cv2.CAP_PROP_FRAME_COUNT))
+
+        self.__handle_count = min(self.__config['frame_count'], self.__capture['frame_count'])
+
+    def __init_surf(self):
+
+        st = time.time()
+        self.__capture['cap'].set(cv2.CAP_PROP_POS_FRAMES, 0)
+        state, first_frame = self.__capture['cap'].read()
+
+        self.__capture['cap'].set(cv2.CAP_PROP_POS_FRAMES, self.__capture['frame_count'] - 20)
+        state, last_frame = self.__capture['cap'].read()
+
+        self.__surf['surf'] = cv2.xfeatures2d.SIFT_create(self.__config['key_point_count'], 1, 1, 1, 1)
+        self.__surf['kp'], self.__surf['des'] = self.__surf['surf'].detectAndCompute(first_frame, None)
+        kp, des = self.__surf['surf'].detectAndCompute(last_frame, None)
+
+        flann = cv2.FlannBasedMatcher(self.__config['index_params'], self.__config['search_params'])
+        matches = flann.knnMatch(self.__surf['des'], des, k=2)
+
+        good_match = []
+        for m, n in matches:
+            if m.distance < self.__config['ratio'] * n.distance:
+                good_match.append(m)
+
+        self.__surf['template_kp'] = []
+        for f in good_match:
+            self.__surf['template_kp'].append(self.__surf['kp'][f.queryIdx])
+
+        self.__capture['cap'].set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+        self.__handle_timer['init'] = int((time.time() - st) * 1000)
+
+        print("[INFO] init time:{}ms".format(self.__handle_timer['init']))
+
+    def __init_data(self):
+        __frame_queue = None
+
+        __write_frame_queue = None
+
+        __surf_list = []
+
+    def __init(self):
+        self.__init_capture()
+        self.__init_surf()
+        self.__init_data()
+
+    def __process(self):
+
+        self.__current_frame = 1
+
+        while True:
+
+            if self.__current_frame > self.__handle_count:
+                break
+
+            start_time = time.time()
+
+            # 抽帧
+            success, frame = self.__capture['cap'].read()
+            self.__handle_timer['read'] = int((time.time() - start_time) * 1000)
+
+            if not success: return
+
+            frame = self.detect_compute(frame)
+
+            st = time.time()
+            self.__capture['video'].write(frame)
+            self.__handle_timer['write'] = int((time.time() - st) * 1000)
+
+            self.__handle_timer['handle'] = int((time.time() - start_time) * 1000)
+
+            self.__current_frame += 1
+
+            self.print_handle_time()
+
+    def stable(self, path):
+        self.__video_path = path
+        self.__init()
+        self.__process()
+
+    def print_handle_time(self):
+        print(
+            "[INFO] handle frame:{}/{} time:{}ms(read:{}ms key:{}ms flann:{}ms matrix:{}ms perspective:{}ms write:{}ms)".
+                format(self.__current_frame,
+                       self.__handle_count,
+                       self.__handle_timer['handle'],
+                       self.__handle_timer['read'],
+                       self.__handle_timer['key'],
+                       self.__handle_timer['flann'],
+                       self.__handle_timer['matrix'],
+                       self.__handle_timer['perspective'],
+                       self.__handle_timer['write']))
+
+    def detect_compute(self, frame):
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        st = time.time()
+        kp, des = self.__surf['surf'].detectAndCompute(frame_gray, None)
+        self.__handle_timer['key'] = int((time.time() - st) * 1000)
+
+        st = time.time()
+        flann = cv2.FlannBasedMatcher(self.__config['index_params'], self.__config['search_params'])
+        matches = flann.knnMatch(self.__surf['des'], des, k=2)
+        self.__handle_timer['flann'] = int((time.time() - st) * 1000)
+
+        st = time.time()
+        good_match = []
+        for m, n in matches:
+            if m.distance < self.__config['ratio'] * n.distance:
+                good_match.append(m)
+
+        p1, p2 = [], []
+        for f in good_match:
+            if self.__surf['kp'][f.queryIdx] in self.__surf['template_kp']:
+                p1.append(self.__surf['kp'][f.queryIdx].pt)
+                p2.append(kp[f.trainIdx].pt)
+
+        H, _ = cv2.findHomography(np.float32(p2), np.float32(p1), cv2.RHO)
+        self.__handle_timer['matrix'] = int((time.time() - st) * 1000)
+
+        st = time.time()
+        output_frame = cv2.warpPerspective(frame, H, self.__capture['size'], borderMode=cv2.BORDER_REPLICATE)
+        self.__handle_timer['perspective'] = int((time.time() - st) * 1000)
+
+        return output_frame
+
+    def detect_perspective(self, frame):
+        """input the current frame, and calculate the projective transformation 
+        matrix for 2D images.
+
+        Args:
+            frame (cv2.ndarray): the frame to be compared.
+
+        Return:
+            H (numpy.ndarray): homography matrix, (3x3).
+        """
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        st = time.time()
+        kp, des = self.__surf['surf'].detectAndCompute(frame_gray, None)
+        self.__handle_timer['key'] = int((time.time() - st) * 1000)
+
+        st = time.time()
+        flann = cv2.FlannBasedMatcher(self.__config['index_params'], self.__config['search_params'])
+        matches = flann.knnMatch(self.__surf['des'], des, k=2)
+        self.__handle_timer['flann'] = int((time.time() - st) * 1000)
+
+        st = time.time()
+        good_match = []
+        for m, n in matches:
+            if m.distance < self.__config['ratio'] * n.distance:
+                good_match.append(m)
+
+        p1, p2 = [], []
+        for f in good_match:
+            if self.__surf['kp'][f.queryIdx] in self.__surf['template_kp']:
+                p1.append(self.__surf['kp'][f.queryIdx].pt)
+                p2.append(kp[f.trainIdx].pt)
+
+        H, _ = cv2.findHomography(np.float32(p2), np.float32(p1), cv2.RHO)
+        self.__handle_timer['matrix'] = int((time.time() - st) * 1000)
+
+        self.print_handle_time()
+
+
+        return H
+
+
+if __name__ == '__main__':
+    s = Stable()
+    s.stable('C:/Users/zying/Desktop/03.mp4')
--- main.py
+++ main.py
@@ -38,7 +38,7 @@ freeze_support()
 
 
 class MyWindow(QMainWindow, QtStyleTools):
-    
+
     def __init__(self):
         super().__init__()
 
@@ -59,7 +59,7 @@ class MyWindow(QMainWindow, QtStyleTools):
         self.loadWorker.sinOut.connect(self.update_load_status)
 
         # 状态栏
-        self.statusBar = self.statusBar() # 状态栏
+        self.statusBar = self.statusBar()  # 状态栏
         # Display cursor coordinates at the right of status bar
         self.label_coordinates = QLabel('Hello')
         self.statusBar.addPermanentWidget(self.label_coordinates)
@@ -79,10 +79,10 @@ class MyWindow(QMainWindow, QtStyleTools):
         self.playTimer = QTimer(self)
         self.playTimer.timeout.connect(self.play_frame)
         self.isPlaying = False
-        self.buttonBackward.pressed.connect(lambda: self.jump_frame(dpos = -5))
-        self.buttonPre.pressed.connect(lambda: self.jump_frame(dpos = -1))
-        self.buttonNext.pressed.connect(lambda: self.jump_frame(dpos = 1))
-        self.buttonForward.pressed.connect(lambda: self.jump_frame(dpos = 5))
+        self.buttonBackward.pressed.connect(lambda: self.jump_frame(dpos=-5))
+        self.buttonPre.pressed.connect(lambda: self.jump_frame(dpos=-1))
+        self.buttonNext.pressed.connect(lambda: self.jump_frame(dpos=1))
+        self.buttonForward.pressed.connect(lambda: self.jump_frame(dpos=5))
 
         # 工具栏
         self.toolBar.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
@@ -90,11 +90,14 @@ class MyWindow(QMainWindow, QtStyleTools):
         self.actionFile.triggered.connect(self.open_file)  # 打开文件
         self.actionGt.triggered.connect(self.load_file)
         self.actionSave.triggered.connect(self.save_file)
+        self.actionEnableStablizer.triggered.connect(self.enable_stable)
         self.actionDict.triggered.connect(self.open_dict)
         self.toolBarVertical.addAction(self.actionZoomIn)
-        self.actionZoomIn.triggered.connect(lambda: self.add_zoom(increment = 10))
+        self.actionZoomIn.triggered.connect(
+            lambda: self.add_zoom(increment=10))
         self.toolBarVertical.addAction(self.actionZoomOut)
-        self.actionZoomOut.triggered.connect(lambda: self.add_zoom(increment = -10))
+        self.actionZoomOut.triggered.connect(
+            lambda: self.add_zoom(increment=-10))
         self.toolBarVertical
         self.toolBarVertical.addWidget(self.zoom_widget)
         self.zoom_widget.setValue(100)
@@ -103,11 +106,12 @@ class MyWindow(QMainWindow, QtStyleTools):
         self.toolBarVertical.addSeparator()
 
         self.actionFit.triggered.connect(self.adjust_scale)
-        
+
         # 标签
-        self.labelHint = ['pedestrian', 'people', 'bicycle', 'car', 'van', 'truck', 'tricycle', 'awning-tricycle', 'bus', 'motor', 'others']
+        self.labelHint = ['pedestrian', 'people', 'bicycle', 'car', 'van',
+                          'truck', 'tricycle', 'awning-tricycle', 'bus', 'motor', 'others']
         self.defaultLabel = self.labelHint[0]
-        self.labelCombobox = DefaultLabelComboBox(self, items = self.labelHint)
+        self.labelCombobox = DefaultLabelComboBox(self, items=self.labelHint)
         self.toolBarVertical.addWidget(self.labelCombobox)
         self.toolBarVertical.addAction(self.actionAnnot)
         self.toolBarVertical.addAction(self.actionDelete)
@@ -124,14 +128,15 @@ class MyWindow(QMainWindow, QtStyleTools):
         # 输入帧数栏
         self.lineCurFrame.returnPressed.connect(self.jump_frame)
         # self.lineCurFrame.textChanged.connect(self.jump_frame)
-        
+
         # 滑动条
         self.vedioSlider.setMinimum(1)
-        self.vedioSlider.sliderMoved.connect(self.move_slider) 
+        self.vedioSlider.sliderMoved.connect(self.move_slider)
         self.vedioSlider.valueChanged.connect(self.move_slider)
 
         # 模型选择框
-        self.model = ["tph_yolov5", "yolox_tiny_vd", "yolox_m_vd", "yolox_l_vd"]
+        self.model = ["tph_yolov5", "yolox_tiny_vd",
+                      "yolox_m_vd", "yolox_l_vd"]
         self.modelDialog = ModelDialog(parent=self, model=self.model)
         self.currentModel = self.modelDialog.currentModel
 
@@ -146,9 +151,13 @@ class MyWindow(QMainWindow, QtStyleTools):
         self.canvas.zoomRequest.connect(self.zoom_request)
         self.prev_label_text = ''
 
+        # sig enable stablizer
+        self.stable = False
+
     # 打开文件
-    def open_file(self): # mp4视频文件
-        self.filePath, _ = QFileDialog.getOpenFileName(self, "Open file", "", "mp4 Video (*.mp4)")
+    def open_file(self):  # mp4视频文件
+        self.filePath, _ = QFileDialog.getOpenFileName(
+            self, "Open file", "", "mp4 Video (*.mp4)")
         if self.filePath.endswith('.mp4'):
             self.videoFileUrl = QUrl.fromLocalFile(self.filePath)
             # 初始化所有图像帧
@@ -164,7 +173,8 @@ class MyWindow(QMainWindow, QtStyleTools):
         self.vedioSlider.setMaximum(self.canvas.numFrames)
 
     def open_dict(self):
-        target_dir_path = ustr(QFileDialog.getExistingDirectory(self, 'Open Directory', '.', QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks))
+        target_dir_path = ustr(QFileDialog.getExistingDirectory(
+            self, 'Open Directory', '.', QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks))
         if not os.path.exists(target_dir_path):
             return
         lst = utils.get_image_list(target_dir_path)
@@ -194,13 +204,16 @@ class MyWindow(QMainWindow, QtStyleTools):
         self.statusBar.showMessage("正在加载标注文件，请稍后")
         self.loadWorker.set_label_type(self.currentLabel)
         if self.currentLabel == 'Yolo':
-            self.labelDir = QFileDialog.getExistingDirectory(self, "Choose annotation Directory" "")
+            self.labelDir = QFileDialog.getExistingDirectory(
+                self, "Choose annotation Directory" "")
             if self.labelDir:
                 # 注意一定要先加载视频
-                self.loadWorker.load_yolo_cfig(self.labelDir, videoWidth=self.videoWidth, videoHeight=self.videoHeight)
+                self.loadWorker.load_yolo_cfig(
+                    self.labelDir, videoWidth=self.videoWidth, videoHeight=self.videoHeight)
                 self.loadWorker.start()
         else:
-            self.labelPath, _ = QFileDialog.getOpenFileName(self, "Choose annotation file", "", "txt(*.txt)")
+            self.labelPath, _ = QFileDialog.getOpenFileName(
+                self, "Choose annotation file", "", "txt(*.txt)")
             if self.labelPath:
                 self.loadWorker.load_path(self.labelPath)
                 self.loadWorker.start()
@@ -216,7 +229,7 @@ class MyWindow(QMainWindow, QtStyleTools):
 
     # TODO 条件：没有文件时，超出范围
     # 跳转到某帧
-    def jump_frame(self, dpos = 0):
+    def jump_frame(self, dpos=0):
         num = int(self.lineCurFrame.text())
         # print("跳转到：", num)
         dpos = int(dpos)
@@ -375,13 +388,13 @@ class MyWindow(QMainWindow, QtStyleTools):
         generate_line_color, generate_fill_color = utils.generate_color_by_text(text)
         shape = self.canvas.set_last_label(text, generate_line_color, generate_fill_color)
         # self.add_label(shape)
-        self.canvas.set_editing(True) # edit mode
+        self.canvas.set_editing(True)  # edit mode
         self.actionAnnot.setEnabled(True)
         # self.set_dirty() # 发生更新，可以保存
 
     def current_path(self):
         return os.path.dirname(self.filePath) if self.filePath else '.'
-    
+
     def set_label_type(self):
         # 这里labelDialog服用了原本ModelDialog类，变量名有点别扭需要注意
         self.labelDialog.pop_up()
@@ -397,13 +410,13 @@ class MyWindow(QMainWindow, QtStyleTools):
             savedPath = self.save_file_dialog(remove_ext=False)
         if savedPath:
             self.save_labels(savedPath)
-    
+
     def save_file_dialog(self, remove_ext=True, dirSave=False):
         if dirSave:
             target_dir_path = ustr(QFileDialog.getExistingDirectory(self, 'Open Directory', '.',
                                                                     QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks))
             return target_dir_path + os.path.sep + \
-                   os.path.basename(os.path.splitext(self.filePath)[0]) + '.txt'
+                os.path.basename(os.path.splitext(self.filePath)[0]) + '.txt'
         else:
             caption = 'Choose Path to save annotation'
             filters = 'Files Directory(*.*)'
@@ -418,11 +431,12 @@ class MyWindow(QMainWindow, QtStyleTools):
             if dlg.exec_():
                 full_file_path = ustr(dlg.selectedFiles()[0])
                 if remove_ext:
-                    return os.path.splitext(full_file_path)[0]  # Return file path without the extension.
+                    # Return file path without the extension.
+                    return os.path.splitext(full_file_path)[0]
                 else:
                     return full_file_path
         return ''
-        
+
     def save_labels(self, savedPath):
         def convert(shape, box):
             dw = 1. / shape[0]
@@ -451,23 +465,27 @@ class MyWindow(QMainWindow, QtStyleTools):
                 savedPathPrefix = savedPath[:-4]
                 if shape.auto == 'M':
                     for i in range(1, self.canvas.numFrames + 1):
-                        savedFramePath = savedPathPrefix + '_' + str(i) + '.txt'
+                        savedFramePath = savedPathPrefix + \
+                            '_' + str(i) + '.txt'
                         min_x, min_y, w, h = convert([self.videoWidth, self.videoHeight],
                                                      [min_x, min_y, w, h])
                         with open(savedFramePath, 'a') as f:
-                            f.write(f"{classId} {min_x:6f} {min_y:.6f} {w:.6f} {h:.6f}\n")
+                            f.write(
+                                f"{classId} {min_x:6f} {min_y:.6f} {w:.6f} {h:.6f}\n")
                 else:
-                    savedFramePath = savedPathPrefix + '_' + str(shape.frameId) + '.txt'
+                    savedFramePath = savedPathPrefix + \
+                        '_' + str(shape.frameId) + '.txt'
                     min_x, min_y, w, h = convert([self.videoWidth, self.videoHeight],
                                                  [min_x, min_y, w, h])
                     with open(savedFramePath, 'a') as f:
-                        f.write(f"{classId} {min_x:6f} {min_y:.6f} {w:.6f} {h:.6f}\n")
+                        f.write(
+                            f"{classId} {min_x:6f} {min_y:.6f} {w:.6f} {h:.6f}\n")
             else:
                 if shape.auto == 'M':
                     for i in range(1, self.canvas.numFrames + 1):
                         results.append(
-                        f"{i},{shape.id},{min_x},{min_y},{w},{h},{shape.score:.2f},{classId},0,0\n"
-                    )
+                            f"{i},{shape.id},{min_x},{min_y},{w},{h},{shape.score:.2f},{classId},0,0\n"
+                        )
                 else:
                     results.append(
                         f"{shape.frameId},{shape.id},{min_x},{min_y},{w},{h},{shape.score:.2f},{classId},0,0\n"
@@ -489,6 +507,9 @@ class MyWindow(QMainWindow, QtStyleTools):
     def closeEvent(self, event):
         sys.exit(0)
 
+    def enable_stable(self):
+        self.stable = not self.stable
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -497,4 +518,3 @@ if __name__ == "__main__":
 
     MyWindow.showMaximized()
     sys.exit(app.exec_())
-
